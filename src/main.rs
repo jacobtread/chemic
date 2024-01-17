eb7c0e21f2df33ef224896f69458309530f57009
Jacobtread <jacobtread@gmail.com>
2024-01-17 21:11:14 +1300

    refactor: clean up device finding and more docs

--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,10 @@ CheMic - Microphone testing tool
 
     let host = cpal::default_host();
 
-    let mut default_input: Option<Device> = None;
-    let mut default_output: Option<Device> = None;
+    let mut input_device: Option<NamedDevice> = None;
+    let mut output_device: Option<NamedDevice> = None;
 
+    // Check for the default mode arg
     let is_default = args()
         .skip(1)
         // Convert to lowercase for case-insensitive matching
@@ -40,28 +41,32 @@ CheMic - Microphone testing tool
 
     // Set the default input devices
     if is_default {
-        default_input = host.default_input_device();
-        default_output = host.default_output_device();
+        input_device = host
+            .default_input_device()
+            // Create a named device
+            .map(NamedDevice::from_default);
+        output_device = host
+            .default_output_device()
+            // Create a named device
+            .map(NamedDevice::from_default);
     }
 
-    let input_device: NamedDevice = default_input
-        // Create a named device
-        .map(NamedDevice::from)
+    // Create the named devices and prompt for them if they are missing
+    let input_device: NamedDevice = input_device
         // Prompt input device if none specified
         .unwrap_or_else(|| {
             prompt_device(&host, "Select input device to test", DeviceType::Input)
                 .expect("Failed to select input device")
         });
 
-    let output_device: NamedDevice = default_output
-        // Create a named device
-        .map(NamedDevice::from)
+    let output_device: NamedDevice = output_device
         // Prompt for an output device if none specified
         .unwrap_or_else(|| {
             prompt_device(&host, "Select output device to play to", DeviceType::Output)
                 .expect("Failed to select output device")
         });
 
+    // Obtain the device configuration
     let input_config: StreamConfig = input_device
         .device
         .default_input_config()
@@ -74,6 +79,7 @@ CheMic - Microphone testing tool
         .expect("No suppoorted output configs")
         .into();
 
+    // Print the device information
     println!("== == == == Input Device == == == ==");
     println!("Name       : {}", input_device.name);
     println!("Channels   : {}", input_config.channels);
@@ -141,42 +147,27 @@ fn start_streams(
     let handle_error = |error: StreamError| eprint!("Error while streaming: {}", error);
 
     // Build the streams
-    let output_stream = match output.build_output_stream(
-        output_config,
-        create_converter_callback(conv),
-        handle_error,
-        None,
-    ) {
-        Ok(value) => value,
-        Err(err) => {
-            eprintln!("Error while starting output stream: {}", err);
-            panic!();
-        }
-    };
+    let output_stream = output
+        .build_output_stream(
+            output_config,
+            create_converter_callback(conv),
+            handle_error,
+            None,
+        )
+        .map_err(io::Error::other)?;
 
-    let input_stream = match input.build_input_stream(
-        input_config,
-        create_producer_callback(producer),
-        handle_error,
-        None,
-    ) {
-        Ok(value) => value,
-        Err(err) => {
-            eprintln!("Error while starting input stream: {}", err);
-            panic!();
-        }
-    };
+    let input_stream = input
+        .build_input_stream(
+            input_config,
+            create_producer_callback(producer),
+            handle_error,
+            None,
+        )
+        .map_err(io::Error::other)?;
 
     // Play the streams
-    if let Err(err) = output_stream.play() {
-        eprintln!("Error while playing output stream: {}", err);
-        panic!();
-    };
-
-    if let Err(err) = input_stream.play() {
-        eprintln!("Error while playing input stream: {}", err);
-        panic!();
-    };
+    output_stream.play().map_err(io::Error::other)?;
+    input_stream.play().map_err(io::Error::other)?;
 
     println!("Playing microphone through output device...");
     println!("Press the ESCAPE or BACKSPACE key to stop..");
@@ -233,8 +224,7 @@ impl NamedDevice {
 }
 
 impl From<Device> for NamedDevice {
-    fn from(value: Device) -> Self {
-        let device = value;
+    fn from(device: Device) -> Self {
         let name = device
             .name()
             // Default "Unknown" name when name cannot be determined
@@ -243,7 +233,8 @@ impl From<Device> for NamedDevice {
     }
 }
 
-/// Type of a device
+/// Type of a [Device]
+#[derive(Clone, Copy)]
 enum DeviceType {
     /// Input device
     Input,
@@ -251,47 +242,70 @@ enum DeviceType {
     Output,
 }
 
-/// Prompts the user for a input device using the provided `prompt`
-/// the `output` option determines whether to include input or output
-/// devices
+/// Finds the default device for the provided `ty` on the `host`
+/// will return [None] if it was unable to find one
+fn get_default_device(host: &Host, ty: DeviceType) -> Option<NamedDevice> {
+    // Type bounds for the default device fn
+    type DefaultDeviceFn = fn(&Host) -> Option<Device>;
+
+    let default_device: DefaultDeviceFn = match ty {
+        DeviceType::Input => Host::default_input_device,
+        DeviceType::Output => Host::default_output_device,
+    };
+
+    default_device(host).map(NamedDevice::from_default)
+}
+
+/// Finds all devices that match the provided `ty` on the `host`
+/// includes a duplicate of the default device
+fn get_devices(host: &Host, ty: DeviceType) -> Vec<NamedDevice> {
+    // Type alias for the filtered device iterator
+    type DevicesFiltered = std::iter::Filter<Devices, fn(&Device) -> bool>;
+    // Type bounds for the devices fn
+    type DevicesFn = fn(&Host) -> Result<DevicesFiltered, DevicesError>;
+
+    // Determine the function for getting the devices of the provided type
+    let devices_fn: DevicesFn = match ty {
+        DeviceType::Input => Host::input_devices,
+        DeviceType::Output => Host::output_devices,
+    };
+
+    // Include the default device as the first device
+    get_default_device(host, ty)
+        .into_iter()
+        // Include all other devices (Duplicate of default device)
+        .chain(
+            devices_fn(host)
+                .expect("Unable to load devices")
+                .map(NamedDevice::from),
+        )
+        .collect()
+}
+
+/// Prompts the user for a device using the provided `prompt` shows
+/// only devices matching the provided `ty` on the `host`
 fn prompt_device(host: &Host, prompt: &str, ty: DeviceType) -> io::Result<NamedDevice> {
+    // Get all available devices
+    let mut devices: Vec<NamedDevice> = get_devices(host, ty);
+
+    // Handle no devices
+    if devices.is_empty() {
+        return Err(io::Error::other("No devices available"));
+    }
+
+    // Create the selection prompt
     let theme = ColorfulTheme::default();
     let mut select = Select::with_theme(&theme);
     select.with_prompt(prompt);
     select.default(0);
     select.report(true);
 
-    let mut devices: Vec<NamedDevice> = Vec::new();
-
-    // Type bounds for the default device fn
-    type DefaultDeviceFn = fn(&Host) -> Option<Device>;
-
-    // Type alias for the filtered device iterator
-    type DevicesFiltered = std::iter::Filter<Devices, fn(&Device) -> bool>;
-
-    // Type bounds for the devices fn
-    type DevicesFn = fn(&Host) -> Result<DevicesFiltered, DevicesError>;
-
-    let (default_device, devices_fn): (DefaultDeviceFn, DevicesFn) = match ty {
-        DeviceType::Input => (Host::default_input_device, Host::input_devices),
-        DeviceType::Output => (Host::default_output_device, Host::output_devices),
-    };
-
-    // Add the default device
-    devices.extend(default_device(host).map(NamedDevice::from_default));
-
-    // Add all devices, includes the default device
-    devices.extend(
-        devices_fn(host)
-            .expect("Unable to load devices")
-            .map(NamedDevice::from),
-    );
-
     // Add a select item for each device
     devices
         .iter()
         .for_each(|device| _ = select.item(&device.name));
 
+    // Select the device
     let index = select.interact()?;
     let device = devices.remove(index);
 
